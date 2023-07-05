a5da903bae178cfbea0284a242c6cb9846c3e997
Apsy_Prakash <apsyajeesh@gmail.com>
2023-07-04 20:43:55 -0700

    add appointment scheduling

---
+++ src/main/resources/static/js/dashboard.js
@@ -0,0 +1,70 @@
+// JavaScript code to load different content based on navigation clicks
+loadAccount();
+
+function loadAccount() {
+  // Perform an AJAX request or update the content directly here
+  $("#dynamicContent").load("account.html");
+}
+
+function loadAppointment() {
+  // Perform an AJAX request or update the content directly here
+    $("#dynamicContent").load("appointment.html", function() {
+        const startDate = document.getElementById('startDate');
+        const startDateListener = function onChange(event) {
+          const selectedDate = event.target.value;
+          loadAppointments(selectedDate);
+          startDate.removeEventListener('change', startDateListener);
+    };
+
+    startDate.addEventListener('change', startDateListener);
+    });
+}
+
+function loadResults() {
+  // Perform an AJAX request or update the content directly here
+  $("#dynamicContent").html("<h3>Results</h3>");
+}
+
+function loadAppointments(date) {
+    const appointmentsContainer = document.getElementById('appointmentsContainer');
+    appointmentsContainer.innerHTML = '';
+    const scheduleBtn = document.getElementById('scheduleBtn');
+    const selectedAppointmentTimes = document.getElementById('selectedAppointmentTimes');
+    const times = ['9:00 AM', '10:00 AM', '11:00 AM', '12:00 PM', '1:00 PM', '2:00 PM', '3:00 PM', '4:00 PM', '5:00 PM'];
+
+    // Create the appointment time cells in a 5x5 format
+    for (let i = 0; i < 5; i++) {
+      const row = document.createElement('div');
+      row.classList.add('row');
+
+      for (let j = 0; j < 5; j++) {
+        const timeIndex = i * 5 + j;
+        if (timeIndex < times.length) {
+          const column = document.createElement('div');
+          column.classList.add('col-2', 'appointment-cell');
+
+          const formCheck = document.createElement('div');
+          formCheck.classList.add('form-check');
+
+          const input = document.createElement('input');
+          input.classList.add('form-check-input');
+          input.type = 'radio';
+          input.name = 'appointmentTime';
+          input.id = `appointmentTime${timeIndex + 1}`;
+          input.value = times[timeIndex];
+
+          const label = document.createElement('label');
+          label.classList.add('form-check-label');
+          label.htmlFor = `appointmentTime${timeIndex + 1}`;
+          label.innerHTML = '&nbsp;' + times[timeIndex];
+
+          formCheck.appendChild(input);
+          formCheck.appendChild(label);
+          column.appendChild(formCheck);
+          row.appendChild(column);
+        }
+      }
+
+      appointmentsContainer.appendChild(row);
+    }
+}
